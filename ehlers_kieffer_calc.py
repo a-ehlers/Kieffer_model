86665c4cd2d3a2bb67c26a949f4bf8158878cabd
a-ehlers <aehlers@vt.edu>
2020-09-25 12:00:20 -0400

    Update code

--- ehlers_kieffer_calc.py
+++ ehlers_kieffer_calc.py
@@ -1,4 +1,8 @@
+## This program is hard-coded with zircon parameters
+
 import math
+
+## Input parameters
 
 Vs = 3.97
 Vp = 8.06
@@ -6,22 +10,31 @@ Vmax = 4.87
 Vol = 130.5
 Z = 2
 
+######################### ACOUSTIC MODES ###############################
+
 u2 = (2/(Vs**(-3) + Vmax**(-3)))**(1/3)
 u1 = (1/(2*(Vs**(-3)) - u2**(-3)))**(1/3)
 u3 = Vp
 
-# print(u2)
-# print(u1)
-# print(u3)
+U_array = [u1, u2, u3]
+W_array = []
 
-acoustic_array = [u1, u2, u3]
-
-print(acoustic_array)
 
 Vmol = Vol*0.6023/Z
 
-print('Molar volume of zircon is: ', Vmol)
+print('Molar volume of zircon is: ', Vmol, 'cm^3')
 
 Kmax = (6*(math.pi)*(math.pi)*(10**24)/Vol)**1/3
 
-print(Kmax)
+print('Radius of Kmax of Brillouin zone (cm-1): ', '{:e}'.format(Kmax))
+
+for i in U_array:
+    W_array.append(132.32*i/(Vol**(1/3)))
+
+    
+print('Directionally-averaged acoustic modes are: ', U_array)
+
+print('Maximum frequencies of acoustic branches are: ', W_array)
+
+
+########################################################################
