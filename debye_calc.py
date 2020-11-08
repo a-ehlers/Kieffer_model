fec1d22dc999f28e82261a6cd0c501be6dac7f03
a-ehlers <aehlers@vt.edu>
2020-11-08 16:28:09 -0500

    Updated with input params

--- debye_calc.py
+++ debye_calc.py
@@ -1,4 +1,42 @@
+## This program is hard-coded with zircon parameters
+
+import math
+
+## Input parameters
+
+Vs = 3.97 # km/s
+Vp = 8.06 # km/s
+Vmax = 4.87 # km/s
+Vol = 130.5 # A
+Z = 2 # molecules/unit cell
+Natoms = 6 # atoms/formula unit
+wc_L = 142
+wc_U = 608
+q_c = 0.22
+wE_1 = 974
+q_1 = 0.055
+wE_2 = 1000
+q_2 = 0.11
+wE_3 = 885
+q_3 = 0.055
+
+ITEMP = 300 # K
+
+## Constants
+AVO = 6.023e23 # 1/mol
+SPEED = 3e10 # m/s
+BOLTZ = 1.38e-23 # J/K
+PLANCK = 6.626e-34 # J.s
+R = 8.3145 # J/mol.K
+CONV = SPEED*PLANCK/BOLTZ
+
 ##################### DEBYE TEMP CALCULATION ###########################
+
+u2 = (2./(Vs**(-3) + Vmax**(-3)))**(1./3.)
+u1 = (1./(2.*(Vs**(-3)) - u2**(-3)))**(1./3.)
+u3 = Vp
+
+Vmol = Vol*0.6023/Z
 
 ## Calculation of Debye temperature (DTEMP)
 
