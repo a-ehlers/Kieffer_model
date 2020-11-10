0f8c6e23dcff14c8304f5e91c63dddb692567915
Robert Weiss <weiszr@vt.edu>
2020-11-10 12:06:57 -0500

    added example

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from Cv_acoustic import Cv_a
-from Cv_optic import Cv_o
-from Cv_einstein import Cv_e
-from params import *
+
+
+def main_func():
+    from src.tools import addition, multip,create_dict
+    params = create_dict()
+    print(params)
+    number_1 = params['Parameter1']
+    number_2 = params['Parameter2']
+    print(number_1)
+    print(number_2)
+    dummy_addition = addition(number_1,number_2)
+    dummy_multip = multip(number_1, number_2)
+    text_a = 'Addition: {t1} + {t2} = {t3}'.format(t1=number_1,t2=number_2, t3=dummy_addition)
+    text_m = 'Multiplication: {t1} * {t2} = {t3}'.format(t1=number_1,t2=number_2, t3=dummy_multip)
+    print(text_a)
+    print(text_m)
+
+
+
 
 if __name__ == '__main__':
-    #calculate heat capacity
-    Cv = Cv_a + Cv_o + Cv_e
-    print('The isochoric heat capacity for zircon at', ITEMP, 'K is', format(Cv, '.2f'), 'J/mol.K')
+    main_func()
---
+++ src/tools.py
@@ -0,0 +1,15 @@
+def create_dict():
+    params_l = {
+            'Parameter1': 1.0,
+            'Parameter2': 3.0
+            }
+    return params_l
+
+
+def addition(a_l,b_l):
+    dummy_l = a_l + b_l
+    return dummy_l
+
+def multip(a_l,b_l):
+    dummy_l = a_l * b_l
+    return dummy_l
