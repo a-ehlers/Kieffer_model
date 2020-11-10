c6acdcaea0082284de4f39c02f8dac795974b14d
a-ehlers <aehlers@vt.edu>
2020-11-09 20:38:42 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     from Cv_acoustic import Cv_a
     from Cv_optic import Cv_o
-    from Cv_einstein import Cv_e
+    from Cv_einstein import Cv_e, ITEMP
     #calculate heat capacity
     Cv = Cv_a + Cv_o + Cv_e
-    print('The isochoric heat capacity for zircon is', Cv, 'J/mol.K')
+    print('The isochoric heat capacity for zircon at', ITEMP, 'K is', format(Cv, '.2f'), 'J/mol.K')
