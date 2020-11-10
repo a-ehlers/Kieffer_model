8a866774838b313c31632e4901684330c83fd888
a-ehlers <aehlers@vt.edu>
2020-11-10 09:02:15 -0500

    Removed list of parameters from Cv_acoustic

--- Cv_acoustic.py
+++ Cv_acoustic.py
@@ -3,33 +3,8 @@
 import math
 from scipy.integrate import quad
 
-## Input parameters
-
-Vs = 3.97 # kilometers/second
-Vp = 8.06 # kilometers/second
-Vmax = 4.87 # kilometers/second
-Vol = 130.5 # A
-Z = 2.0 # molecules/unit cell
-Natoms = 6.0 # atoms/formula unit
-wc_L = 142.0
-wc_U = 608.0
-q_c = 0.22
-wE_1 = 974.0
-q_1 = 0.055
-wE_2 = 1000.0
-q_2 = 0.11
-wE_3 = 885.0
-q_3 = 0.055
-
-ITEMP = 300.0 # Kelvin
-
-## Constants
-AVO = 6.023e23 # 1/mole
-SPEED = 3e10 # meters/second
-BOLTZ = 1.38e-23 # Joules/Kelvin
-PLANCK = 6.626e-34 # Joules.second
-R = 8.3145 # Joules/mole.Kelvin
-CONV = SPEED*PLANCK/BOLTZ
+#import params.py for all input parameters
+from params import *
 
 ######################### ACOUSTIC MODES ###############################
 
