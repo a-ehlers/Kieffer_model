7dc3764c960e3261a485c5ced9cbf2ac51a40c6b
a-ehlers <aehlers@vt.edu>
2020-12-01 08:47:18 -0500

    edited S_einstein.py

--- src/S_einstein.py
+++ src/S_einstein.py
@@ -11,10 +11,6 @@ from params import *
 
 S_e = []
 
-S_e1 = []
-
-S_e2 = []
-
 WE_array = [wE_1, wE_2, wE_3]
 
 for i in ATEMP:
@@ -25,8 +21,6 @@ for i in ATEMP:
 
     einstein_S_1 = 3.*AVO*BOLTZ*(q_1*ein_array_1[0] + q_2*ein_array_1[1] + q_3*ein_array_1[2])
 
-    S_e1.append(einstein_S_1)
-
     ein_array_2 = []
     
     for j in WE_array:
@@ -34,9 +28,5 @@ for i in ATEMP:
 
     einstein_S_2 = 3.*AVO*BOLTZ*(q_1*ein_array_2[0] + q_2*ein_array_2[1] + q_3*ein_array_2[2])
 
-    S_e2.append(einstein_S_2)
+    S_e.append(einstein_S_1 - einstein_S_2)
 
-for i in range(len(ATEMP)):
-    S_e.append(S_e1[i] - S_e2[i])
-
-##print(S_e)
